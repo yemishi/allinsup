6ef4f279fd296c1e506eaa94712182a3abc42024
Davi Damião <daviddperera10@gmail.com>
2024-01-05 13:22:01 +0000

    updatef

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.css'
 import Routes from './routes/index.tsx'
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+
     <QueryClientProvider client={queryClient}>
       <Routes />
     </QueryClientProvider>
-  </React.StrictMode>
 )
