6e6af2fedb639c0f6d34dfe701a6ea2481559d95
Davi Damião <daviddperera10@gmail.com>
2024-01-07 14:59:03 +0000

    updated

--- frontend/src/pages/Orders.tsx
+++ frontend/src/pages/Orders.tsx
@@ -11,7 +11,7 @@ export default function Orders() {
         "orders",
         async () => {
             const response = await axiosRequest.getOrders();
-            console.log(response)
+            console.log('response')
             return response.data;
         },
         {
@@ -19,7 +19,6 @@ export default function Orders() {
             retryDelay: 100,
         }
     );
-
 
     useEffect(() => {
         if (data) {
@@ -44,11 +43,11 @@ export default function Orders() {
                     <Link to={`/orderInfo/${orderId}`} className="ml-auto px-2 py-1  border mt-auto border-gray-500  font-serif font-thin rounded-md">Detalhes</Link>
                 </div>
             }) : <div className="flex flex-col items-center justify-center mt-10 mb-16 ">
-<svg viewBox="0 -3 24 24" className="w-32 fill-slate-700"  xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" ></g><g id="SVGRepo_tracerCarrier" ></g><g id="SVGRepo_iconCarrier"><path  d="M7 0V6C7 6.74338 7.78231 7.2269 8.44721 6.89443L12 5.11803L15.5528 6.89443C16.2177 7.2269 17 6.74338 17 6V0H22C23.1046 0 24 0.89543 24 2V16C24 17.1046 23.1046 18 22 18H2C0.89543 18 0 17.1046 0 16V2C0 0.89543 0.89543 0 2 0H7zM9 0H15V4.38197L12.4472 3.10557C12.1657 2.96481 11.8343 2.96481 11.5528 3.10557L9 4.38197V0z" ></path></g></svg>
+                <svg viewBox="0 -3 24 24" className="w-32 fill-slate-700" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" ></g><g id="SVGRepo_tracerCarrier" ></g><g id="SVGRepo_iconCarrier"><path d="M7 0V6C7 6.74338 7.78231 7.2269 8.44721 6.89443L12 5.11803L15.5528 6.89443C16.2177 7.2269 17 6.74338 17 6V0H22C23.1046 0 24 0.89543 24 2V16C24 17.1046 23.1046 18 22 18H2C0.89543 18 0 17.1046 0 16V2C0 0.89543 0.89543 0 2 0H7zM9 0H15V4.38197L12.4472 3.10557C12.1657 2.96481 11.8343 2.96481 11.5528 3.10557L9 4.38197V0z" ></path></g></svg>
                 <p className="text-gray-600 text-lg text-center">
                     Você não possui nenhum pedido ainda.
                 </p>
-            
+
             </div>}
 
         </div>
