b82dc0ca717c922d6461518c9ec10e88bd71217f
Davi Damião <daviddperera10@gmail.com>
2024-01-07 01:02:52 +0000

    updated

--- frontend/src/pages/Orders.tsx
+++ frontend/src/pages/Orders.tsx
@@ -2,16 +2,12 @@ import { useEffect, useState, } from "react"
 import { OrderType } from "../types"
 import { axiosRequest } from "../components"
 import { divList } from "../utils"
-import { useNavigate } from "react-router-dom"
-import { useGlobalState } from "../App"
 import { useQuery } from "react-query"
 import { Link } from "react-router-dom"
 
 export default function Orders() {
-    const navigate = useNavigate()
-    const { dispatch } = useGlobalState()
     const [orders, setOrders] = useState<OrderType[]>()
-    const { data, error } = useQuery(
+    const { data } = useQuery(
         "orders",
         async () => {
             const response = await axiosRequest.getOrders();
@@ -22,10 +18,7 @@ export default function Orders() {
             retryDelay: 100,
         }
     );
-    if (error) {
-        dispatch({ type: "SET_USER_OPEN", payload: true })
-        navigate('/')
-    }
+
 
     useEffect(() => {
         if (data) {
@@ -36,7 +29,7 @@ export default function Orders() {
     return (
         <div className="flex flex-col items-center p-4 text-white gap-5">
             <h1 className="text-xl mt-4 self-baseline font-anton font-semibold">Meus pedidos</h1>
-            {orders && orders.map((order, index) => {
+            {orders ? orders.map((order, index) => {
                 const { orderId, price, purchaseDate, status } = order
                 return <div key={`${order}_${index}`} className="p-4  border font-lato text-gray-200 flex w-full rounded bg-primary-550
                 border-gray-600 ">
@@ -49,10 +42,16 @@ export default function Orders() {
                     </dl>
                     <Link to={`/orderInfo/${orderId}`} className="ml-auto px-2 py-1  border mt-auto border-gray-500  font-serif font-thin rounded-md">Detalhes</Link>
                 </div>
-            })}
-            {!orders && <div>
-                siuuuuu
+            }) : <div className="flex flex-col items-center justify-center mt-10 mb-16 ">
+                <img className="w-40 h-40 mb-6" src="./public/noOrder.svg" alt="sem pedido ilustracao" />
+                <p className="text-gray-600 text-lg text-center">
+                    Você não possui nenhum pedido ainda.
+                </p>
+                <button >
+
+                </button>
             </div>}
+
         </div>
     )
 }
