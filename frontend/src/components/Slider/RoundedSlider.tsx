a248bd48c760d663f38e53b09154ce1be50afc9b
Davi Damião <daviddperera10@gmail.com>
2024-01-13 03:29:04 +0000

    updated

--- frontend/src/components/Slider/RoundedSlider.tsx
+++ frontend/src/components/Slider/RoundedSlider.tsx
@@ -54,9 +54,10 @@ export default function RoundedSlider({ children }: PropsType) {
                 const current = state.brandHome === brands[name] || state.brandHome === name
 
                 return <Slide key={name} onClick={() => dispatch({ type: "SET_BRAND_HOME", payload: brands[name] })} className={`max-w-[110px] group
-                !flex cursor-pointer ml-auto items-center flex-col gap-3 hover:text-secondary-200 duration-200 font-lato text-sm md:max-w-[130px] ${current ? "text-secondary-200" : "text-white"}`}>
+                !flex cursor-pointer ml-auto items-center flex-col gap-3 hover:text-secondary-200 duration-200 font-lato text-sm md:max-w-[130px]
+                m:font-semibold md:text-lg  ${current ? "text-secondary-200" : "text-white"}`}>
 
-                    <div className={`h-[110px] w-[110px] overflow-hidden duration-200 bg-primary rounded-full border-2 
+                    <div className={`h-[110px] w-[110px] lg:h-[125px] lg:w-[125px] overflow-hidden duration-200 bg-primary rounded-full border-2 
                        ${current ? "border-secondary-200 border-[3px]" : "border-primary-200  group-hover:border-secondary-200 group-hover:border-opacity-50 group-hover:border-[3px]"}`}>
                         <img src={banner} className="duration-300 group-hover:scale-105 rounded-full h-full w-full object-cover border-primary-500 border-2 " alt={parseAlt(banner)} />
                     </div>
