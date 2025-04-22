d727f8b8642ae8824d3f9fca821f59c5ff95824d
yemishi <daviddperera10@gmail.com>
2025-04-22 20:59:38 +0100

    updated signin/signup forms using useForm custom hook and improved UI

--- frontend/src/components/form/user/SignInForm.tsx
+++ frontend/src/components/form/user/SignInForm.tsx
@@ -1,103 +1,19 @@
-import { HTMLAttributes, useState } from "react";
-
-import { z } from "zod";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { IoCloseSharp } from "react-icons/io5";
-import { updateToken } from "../../../services/axios.config";
-import Input from "../../ui/Input";
-import Button from "../../ui/Button";
-import axiosRequest from "../../../services/axios.config";
+import Form, { InputsType } from "../Form";
+import axiosRequest, { updateToken } from "../../../services/axios.config";
 import { toast } from "react-toastify";
-import { cleanClasses } from "../../../utils/helpers";
 
-interface FormProps extends HTMLAttributes<HTMLFormElement> {
-  openSignUp: () => void;
-  onClose: () => void;
-  disableExit?: boolean;
-  onSuccess?: () => void;
-}
-
-export default function SignInForm({ onClose, openSignUp, disableExit, onSuccess, ...props }: FormProps) {
-  type RegisterInputsType = z.infer<typeof FormSchema>;
-  const { className = "", ...rest } = props;
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  const FormSchema = z.object({
-    email: z.string().min(1, "this field has to be filled.").email("this is not a valid email."),
-    password: z.string().min(6, "this field must have at least 6 letters."),
-  });
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<RegisterInputsType>({ resolver: zodResolver(FormSchema) });
-  const onSubmit: SubmitHandler<RegisterInputsType> = async (values) => {
-    setIsLoading(true);
-    await FormSchema.parseAsync(values);
-    const response = await axiosRequest.user.login(values.email, values.password);
-    setIsLoading(false);
-    if (response.error) return toast.error(response.message);
-    return updateToken(response.token), toast.success(response.message), onClose(), onSuccess && onSuccess();
+export default function SigninForm({ onClose, onSuccess }: { onClose: () => void; onSuccess?: () => void }) {
+  const inputs: InputsType = {
+    email: { label: "Email", value: "", min: 1, isEmail: true, inputProps: { placeholder: "mrMuscle@gmail.com" } },
+    password: { label: "Password", value: "", min: 6, isPassword: true, inputProps: { placeholder: "mrMuscle123" } },
   };
-
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className={cleanClasses(
-        className,
-        "bg-primary-600 rounded-lg shadow-xl w-full h-full flex flex-col gap-3 p-4"
-      )}
-      {...rest}
-    >
-      <div className="flex justify-center items-center relative">
-        <span className="font-montserrat self-center text-3xl font-semibold mb-10">Sign in</span>
-        {!disableExit && (
-          <button type="button" onClick={onClose}>
-            <IoCloseSharp className="h-8 w-8 top-0 right-0 absolute" />
-          </button>
-        )}
-      </div>
-
-      <Input
-        {...register("email")}
-        disabled={isLoading}
-        autoComplete="email"
-        error={errors.email?.message}
-        name="email"
-        type="email"
-        label="Email"
-        id="email"
-        placeholder="mrMuscle@gmail.com"
-      />
-      <Input
-        {...register("password")}
-        disabled={isLoading}
-        autoComplete="password"
-        error={errors.password?.message}
-        isPassword={true}
-        label="Password"
-        name="password"
-        id="password"
-        placeholder="muscle123"
-      />
-      <Button disabled={isLoading} type="submit" className="self-center mt-16">
-        Login
-      </Button>
-
-      <div className="self-center font-bold space-x-1 mt-auto">
-        <div className="h-[1px] w-full bg-zinc-700 mb-5" />
-        <span className="text-white text-opacity-50">Don't have an account</span>
-        <button
-          disabled={isLoading}
-          type="button"
-          onClick={openSignUp}
-          className="underline underline-offset-2 disabled:pointer-events-none disabled:animate-pulse"
-        >
-          Sign up here
-        </button>
-      </div>
-    </form>
-  );
+  const onSubmit = async (values: { email: string; password: string }) => {
+    const response = await axiosRequest.user.login(values.email, values.password);
+    if (response.error) return toast.error(response.message);
+    updateToken(response.token);
+    toast.success(response.message);
+    if (onSuccess) onSuccess();
+    onClose();
+  };
+  return <Form onSubmit={onSubmit} inputs={inputs} />;
 }
