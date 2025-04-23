9393ee97a75d8ad1fc7d9ea19b4d446c64f23b9b
yemishi <daviddperera10@gmail.com>
2025-04-23 20:43:36 +0100

    updated ui

--- frontend/src/features/Checkout/Summary.tsx
+++ frontend/src/features/Checkout/Summary.tsx
@@ -1,65 +1,54 @@
-import { DivDraggable, Image } from "../../components";
+import { Image } from "../../components";
+import MotionDiv from "../../components/ui/MotionDiv";
 import { CartType } from "../../types/response";
 import { parseLocalCurrency } from "../../utils/formatting";
 
-export default function Summary({
-  cart,
-  method,
-}: {
-  cart: CartType[];
-  method: string;
-}) {
-  const totalPrice = cart.reduce(
-    (prev, curr) => prev + curr.price * curr.amount,
-    0
-  );
-  return (
-    <DivDraggable
-      exit="-100%"
-      initialDirection="100%"
-      removeAnimatePresence
-      disableDrag
-      className=" w-full"
-    >
-      <h2 className="font-anton text-xl p-2 w-max text-gray-200 md:self-center lg:text-2xl">
-        Summary
-      </h2>
-      <div className="flex font-bold text-secondary-200 mt-3 md:self-center md:gap-10 md:text-lg lg:text-xl">
-        <span>{method}</span>
-        <span className="ml-auto">{parseLocalCurrency(totalPrice)}</span>
-      </div>
-      <table className="flex items-center justify-center w-full">
-        <tbody className="flex flex-col gap-5 pt-4 w-full rounded  md:grid md:grid-cols-2 ">
-          {cart.map((product, index) => {
-            const { amount, coverPhoto, name, price } = product;
-            const totalPrice = price * amount;
-            return (
-              <tr
-                key={`${name}_${index}`}
-                className="text-gray-200 flex md:flex-col gap-5 pr-2 
-                        w-full border-b border-primary-200 bg-primary-550 border lg:text-lg rounded-lg p-2"
-              >
-                <td className="flex items-center bg-white p-2 rounded-md md:max-w-full  justify-center min-w-[40%] max-w-[40%]">
-                  <Image
-                    className="bg-white h-36  object-contain lg:h-44"
-                    src={coverPhoto}
-                  />
-                </td>
+export default function Summary({ cart, method }: { cart: CartType[]; method: string }) {
+  const totalPrice = cart.reduce((prev, curr) => prev + curr.price * curr.amount, 0);
 
-                <td>
-                  <div className="flex flex-col h-full gap-2 font-anton overflow-hidden py-3 ">
-                    <span className="font-semibold mr-5">{name}</span>
-                    <span className="font-bold text-secondary-500 mt-auto">
-                      {parseLocalCurrency(totalPrice)}
-                    </span>
-                    <span className="">Qtd: {amount}</span>
-                  </div>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </DivDraggable>
+  return (
+    <MotionDiv reverse className="lg:min-w-[500px] max-w-2xl flex flex-col gap-3">
+      <div className="flex font-bold text-secondary-200 mt-3 md:self-center md:gap-10 md:text-lg lg:text-xl w-full justify-between">
+        <div className="flex gap-1">
+          <span className="text-white">Payment method:</span>
+          <span className="first-letter:uppercase">{method}</span>
+        </div>
+
+        <div className="flex gap-1">
+          <span className="text-white">Price total:</span>
+          <span className="first-letter:uppercase">{parseLocalCurrency(totalPrice)}</span>
+        </div>
+      </div>
+
+      <div className="max-h-[500px] overflow-y-auto w-full">
+        <table className="flex items-center justify-center w-full  ">
+          <tbody className="flex flex-col gap-5 pt-4 w-full rounded  md:grid md:grid-cols-2 ">
+            {cart.map((product, index) => {
+              const { amount, coverPhoto, name, price } = product;
+              const totalPrice = price * amount;
+              return (
+                <tr
+                  key={`${name}_${index}`}
+                  className="text-gray-200 flex md:flex-col gap-5 pr-2 
+              w-full border-b border-primary-200 bg-primary-550 border lg:text-lg rounded-lg p-2"
+                >
+                  <td className="flex items-center bg-white p-2 rounded-md md:max-w-full  justify-center min-w-[40%] max-w-[40%]">
+                    <Image className="bg-white h-36  object-contain lg:h-44" src={coverPhoto} />
+                  </td>
+
+                  <td>
+                    <div className="flex flex-col h-full gap-2 font-anton overflow-hidden py-3 ">
+                      <span className="font-semibold mr-5">{name}</span>
+                      <span className="font-bold text-secondary-500 mt-auto">{parseLocalCurrency(totalPrice)}</span>
+                      <span className="">Qtd: {amount}</span>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </MotionDiv>
   );
 }
