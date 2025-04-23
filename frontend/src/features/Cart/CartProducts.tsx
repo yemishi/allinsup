691843991b109aaec62ce0ac36e3e6a331573cd1
yemishi <daviddperera10@gmail.com>
2025-04-23 20:47:16 +0100

    improved code and ui

--- frontend/src/features/Cart/CartProducts.tsx
+++ frontend/src/features/Cart/CartProducts.tsx
@@ -18,8 +18,7 @@ export default function CartProducts() {
   };
 
   const handleChange = (value: string, stock: number, productIndex: number) => {
-    const updatedValue =
-      Number(value) > stock ? stock : Number(value) < 1 ? 1 : Number(value);
+    const updatedValue = Number(value) > stock ? stock : Number(value) < 1 ? 1 : Number(value);
     updateAmount(updatedValue, productIndex);
   };
   const remove = (productIndex: number) => {
@@ -29,18 +28,14 @@ export default function CartProducts() {
   return (
     <div className="flex flex-col p-3 font-anton">
       {cart.map((product, index) => {
-        const { name, coverPhoto, price, promotion, amount, _id, stock } =
-          product;
+        const { name, coverPhoto, price, promotion, amount, _id, stock } = product;
         return (
           <div
             key={`${product._id}_${name}_${price}`}
             className="flex overflow-hidden flex-row gap-3 h-[170px] border-b pr-3 border-zinc-700 py-6 pb-5 relative"
           >
             <Link className="!flex" to={`/product/${_id}`}>
-              <div
-                onClick={() => updateAmount(amount + 1, index)}
-                className="w-auto flex bg-white p-2 rounded-md"
-              >
+              <div onClick={() => updateAmount(amount + 1, index)} className="w-auto flex bg-white p-2 rounded-md">
                 <Image
                   className="object-contain !min-w-[120px] !max-w-[120px] hover:scale-110 duration-300"
                   src={coverPhoto}
@@ -48,31 +43,26 @@ export default function CartProducts() {
               </div>
             </Link>
 
-            <div className="flex flex-col text-sm lg:text-lg w-full">
-              <span className="hover:text-secondary-500 duration-300">
-                {name}
-              </span>
+            <div className="flex flex-col text-sm pr-5 lg:text-lg w-full">
+              <span className="hover:text-secondary-500 duration-300 ">{name}</span>
 
-              <button
-                onClick={() => remove(index)}
-                className="absolute top-2 right-0 cursor-pointer self-start"
-              >
-                <IoIosClose className="h-7 w-7 md:w-9 md:h-9" />
+              <button onClick={() => remove(index)} className="absolute top-2 right-0 cursor-pointer self-start">
+                <IoIosClose className="h-7 w-7 md:w-9 md:h-9 text-red-500" />
               </button>
 
-              <p>Qtd:{amount}</p>
+              <div>
+                Qtd:
+                <span className="text-secondary-300 font-semibold">{amount}</span>
+              </div>
 
               <div className="flex justify-between mt-auto items-end">
                 <p className="text-secondary-500 font-bold text-lg lg:text-xl">
-                  {parseLocalCurrency(
-                    promotion ? Number(promotion) : Number(price)
-                  )}
+                  {parseLocalCurrency(promotion ? Number(promotion) : Number(price))}
                 </p>
-                <span className="flex border-gray-600 p-2 rounded-lg gap-2 border absolute right-0">
+                <span className="flex border-gray-600 p-2 rounded-lg gap-2 border absolute right-2">
                   <button
                     onClick={() => updateAmount(amount - 1, index)}
-                    className={`w-5 md:w-8 ${amount - 1 < 1 ? "pointer-events-none opacity-50" : ""
-                      }`}
+                    className={`w-5 md:w-8 ${amount - 1 < 1 ? "pointer-events-none opacity-50" : ""}`}
                   >
                     <FiMinus className="!h-full !w-full" />
                   </button>
@@ -90,8 +80,7 @@ export default function CartProducts() {
 
                   <button
                     onClick={() => updateAmount(amount + 1, index)}
-                    className={`w-5 md:w-8 ${amount + 1 > stock ? "pointer-events-none opacity-50" : ""
-                      }`}
+                    className={`w-5 md:w-8 ${amount + 1 > stock ? "pointer-events-none opacity-50" : ""}`}
                   >
                     <IoIosAdd className="!w-full !h-full" />
                   </button>
