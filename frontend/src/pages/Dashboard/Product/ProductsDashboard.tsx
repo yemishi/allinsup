29f6de37e1e2fc1025c8066ecfab2d2c5d3acbaa
yemishi <daviddperera10@gmail.com>
2025-04-28 00:38:29 +0100

    updated ui with new components and refactored code

--- frontend/src/pages/Dashboard/Product/ProductsDashboard.tsx
+++ frontend/src/pages/Dashboard/Product/ProductsDashboard.tsx
@@ -1,24 +1,17 @@
-import axios from "axios";
-
 import { ReactNode, useState } from "react";
 import useScrollQuery from "../../../hooks/useInfiniteQuery";
-import { ProductFormType, ProductType, VariantType } from "../../../types/response";
+import { ProductType } from "../../../types/response";
 
 import axiosRequest from "../../../services/axios.config";
 import { useSearchParams } from "react-router-dom";
-import { enableScroll, productDetails } from "../../../utils/helpers";
+import { productDetails } from "../../../utils/helpers";
 import { toast } from "react-toastify";
 
-import { Modal, ProductForm } from "../../../components";
-import { DivDraggable, MotionDiv, Button } from "../../../ui";
-
-interface VariantData extends Omit<VariantType, "photos"> {
-  photos: [];
-  photosData: string[];
-}
-type ProductEditType = Omit<ProductType, "variants"> & {
-  variants: VariantData[];
-};
+import { ErrorWrapper, Modal } from "../../../components";
+import { MotionDiv, Button } from "../../../ui";
+import ProductForm from "./productForm/ProductForm";
+import updateVariantsPics from "./productForm/updateVariantsPics";
+import { IoIosAddCircle } from "react-icons/io";
 
 export default function ProductsDashboard() {
   const [searchParams] = useSearchParams();
@@ -26,9 +19,6 @@ export default function ProductsDashboard() {
   const [isModal, setIsModal] = useState<ReactNode | false>(false);
   const closeModal = () => setIsModal(false);
 
-  const closePopUp = () => {
-    enableScroll(), closeModal();
-  };
   const {
     values: products,
     isLoading,
@@ -36,40 +26,36 @@ export default function ProductsDashboard() {
     hasNextPage,
     ref,
     refetch,
+    hasError,
   } = useScrollQuery<ProductType>({
     queryKey: ["dashboard-products", query],
     url: `/product?query=${query || ""}`,
   });
   if (isLoading) return <img src="/loading.svg" className="self-center" alt="loading icon" />;
-
   const deleteProduct = async (productId: string) => {
     const fetchData = async () => {
       const { error, message } = await axiosRequest.product.delete(productId);
       if (error) return toast.error(message);
-      toast.success(message), refetch(), closePopUp();
+      toast.success(message), refetch(), closeModal();
     };
     setIsModal(<DeleteProduct fetchData={fetchData} close={closeModal} />);
   };
 
   const newProduct = () => {
-    const fetchData = async (values: ProductFormType) => {
-      await updatedVariantsPhotos(values);
+    const fetchData = async (values: ProductType) => {
+      await updateVariantsPics(values);
       const { error, message } = await axiosRequest.product.create(values as any);
       if (error) {
         toast.error(message);
         return;
       }
-      toast.success(message), closePopUp(), refetch();
+      toast.success(message), closeModal(), refetch();
     };
-    setIsModal(
-      <DivDraggable maxMd initialDirection="100%" closeParent={closePopUp}>
-        <ProductForm action={fetchData} onClose={closeModal} />
-      </DivDraggable>
-    );
+    setIsModal(<ProductForm title="New Product" action={fetchData} onClose={closeModal} />);
   };
-  const editProduct = (product: ProductEditType, productId: string) => {
-    const fetchData = async (form: ProductFormType, photosDelete?: string[]) => {
-      const variants = await updatedVariantsPhotos(form, photosDelete);
+  const editProduct = (product: ProductType, productId: string) => {
+    const fetchData = async (form: ProductType, photosDelete?: string[]) => {
+      const variants = await updateVariantsPics(form, photosDelete);
       form.variants = variants as any;
       const updatedForm = {
         ...form,
@@ -85,74 +71,64 @@ export default function ProductsDashboard() {
         toast.error(message);
         return;
       }
-      toast.success(message), close(), refetch();
+      toast.success(message), closeModal(), refetch();
     };
-    setIsModal(
-      <DivDraggable maxMd initialDirection="100%" closeParent={close}>
-        <ProductForm action={fetchData} onClose={closePopUp} defaultValues={product} />
-      </DivDraggable>
-    );
+    setIsModal(<ProductForm title="Edit Product" action={fetchData} onClose={closeModal} product={product} />);
   };
 
   return (
     <div className="flex flex-col gap-4 p-4 md:w-[80%] self-center">
-      <Button
-        onClick={newProduct}
-        type="button"
-        className="py-2 px-4 my-2 rounded-full bg-primary-400 border self-end "
-      >
-        +
-      </Button>
-      {isModal && (
-        <Modal className="mx-auto my-auto" onClose={closeModal}>
-          {isModal}
-        </Modal>
-      )}
-      <div className="flex flex-wrap text-gray-200 gap-4 items-center justify-center">
-        {products.map((product) => {
-          const { coverPhoto, name } = productDetails(product);
-          const variantsClean = product.variants.map((v) => ({
-            ...v,
-            photosData: v.photos,
-            photos: [],
-          })) as VariantData[];
-          const productEdit = {
-            ...product,
-            variants: variantsClean,
-          };
+      <ErrorWrapper error={hasError} refetch={refetch}>
+        <button onClick={newProduct} type="button" className="sticky top-5">
+          <IoIosAddCircle
+            className="w-10 h-10 md:h-12 md:w-12 ml-auto text-secondary-100 hover:brightness-110 hover:scale-105
+           active:scale-95 duration-100 "
+          />
+        </button>
 
-          return (
-            <div
-              className="flex flex-col gap-3 w-full max-w-[250px] border border-primary-200 p-2 max-w-44  h-72 md:h-80 overflow-hidden min-w-[170px] bg-primary-550 rounded-lg md:min-w-[195px] md:max-w-[270px]"
-              key={`${product._id}_${name}`}
-            >
-              <div className=" h-40 md:h-48 flex p-2 bg-white rounded-lg">
-                <img src={coverPhoto} className="w-full object-contain" alt="" />
+        {isModal && (
+          <Modal className="mx-auto my-auto" onClose={closeModal}>
+            {isModal}
+          </Modal>
+        )}
+        <div className="flex flex-wrap text-gray-200 gap-4 items-center justify-center">
+          {products.map((product) => {
+            const { coverPhoto, name } = productDetails(product);
+
+            return (
+              <div
+                className="flex flex-col gap-3 w-full max-w-[250px] border border-primary-200 p-2 max-w-44 h-72 md:h-80 overflow-hidden min-w-[170px]
+                 bg-primary-550 rounded-lg md:min-w-[195px] md:max-w-[270px]"
+                key={`${product._id}_${name}`}
+              >
+                <div className=" h-40 md:h-48 flex p-2 bg-white rounded-lg">
+                  <img src={coverPhoto} className="w-full object-contain" alt="" />
+                </div>
+                <div className="w-full pl-2 pr-1">
+                  <span className="font-lato font-semibold text-sm md:text-lg line-clamp-2">{name}</span>
+                </div>
+
+                <span className="mt-auto grid grid-cols-2 gap-2">
+                  <Button
+                    onClick={() => deleteProduct(product._id)}
+                    className="mt-auto p-1 rounded-lg font-lato font-bold bg-gray-300 text-black"
+                  >
+                    Delete
+                  </Button>
+                  <Button
+                    onClick={() => editProduct(product, product._id)}
+                    className="mt-auto bg-secondary-600 p-1 rounded-lg font-lato font-bold"
+                  >
+                    Update
+                  </Button>
+                </span>
               </div>
-              <div className="w-full pl-2 pr-1">
-                <span className="font-lato font-semibold text-sm md:text-lg">{name}</span>
-              </div>
+            );
+          })}
 
-              <span className="mt-auto grid grid-cols-2 gap-2">
-                <Button
-                  onClick={() => deleteProduct(product._id)}
-                  className="mt-auto p-1 rounded-lg font-lato font-bold "
-                >
-                  Delete
-                </Button>
-                <Button
-                  onClick={() => editProduct(productEdit, product._id)}
-                  className="mt-auto bg-secondary-600 p-1 rounded-lg font-lato font-bold"
-                >
-                  Update
-                </Button>
-              </span>
-            </div>
-          );
-        })}
-
-        {!isFetchingNextPage && hasNextPage && <div ref={ref} />}
-      </div>
+          {!isFetchingNextPage && hasNextPage && <div ref={ref} />}
+        </div>
+      </ErrorWrapper>
     </div>
   );
 }
@@ -171,33 +147,4 @@ const DeleteProduct = ({ fetchData, close }: { fetchData: () => void; close: () 
       </div>
     </MotionDiv>
   );
-};
-
-const updatedVariantsPhotos = async (form: ProductFormType, photosDelete?: string[]) => {
-  const variants = await Promise.all(
-    form.variants.flatMap(async (variant) => {
-      const formData = new FormData();
-
-      variant.photos.forEach((photo) => formData.append(`files[]`, photo[0]));
-      const response = await axios
-        .post(`${import.meta.env.VITE_API_URL}/uploadImage/many`, formData)
-        .then((res) => res.data);
-      if (response.error) return toast.error(response.message);
-      const photosDataUrl = variant.photosData && variant.photosData.length > 0 && [...variant.photosData];
-      variant.photos = [];
-      if (photosDataUrl) {
-        const urls =
-          photosDelete && photosDelete?.length > 0
-            ? [...photosDataUrl.filter((i) => !photosDelete.includes(i))]
-            : [...photosDataUrl];
-        variant.photos = urls as any;
-      }
-      variant.photos = [...variant.photos, ...response.urls];
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { photosData, ...rest } = variant;
-      variant = rest;
-      return variant;
-    })
-  );
-  return variants;
 };
