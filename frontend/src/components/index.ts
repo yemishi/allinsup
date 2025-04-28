03e7f13c264b96b3f88c2b7be5a8bd568a01bf44
yemishi <daviddperera10@gmail.com>
2025-04-28 23:54:47 +0100

    add .tsx extensions to all component imports and exports for build compatibility

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,30 +1,51 @@
-import ProductGrid from "./Card/CardGrid";
+import ProductGrid from "./Card/CardGrid.tsx";
 import { SwiperSlide, SwiperProps as SliderProps, Swiper } from "swiper/react";
 
-export { SwiperSlide as Slide, ProductGrid, Swiper };
-export { default as Form } from "./Form/Form.tsx";
+import Form from "./Form/Form.tsx";
+import SignInForm from "./Form/User/SignInForm.tsx";
+import SignUpForm from "./Form/User/SignUpForm.tsx";
+import Login from "./Form/User/Login.tsx";
+import UserAddressForm from "./Form/User/UserAddressForm.tsx";
 
-export { default as SignInForm } from "./Form/User/SignInForm.tsx";
-export { default as SignUpForm } from "./Form/User/SignUpForm.tsx";
-export { default as Login } from "./Form/User/Login.tsx";
-export { default as UserAddressForm } from "./Form/User/UserAddressForm.tsx";
+import Card from "./Card/Card.tsx";
+import CardSkeleton from "./Card/CardSkeleton.tsx";
+import CardGrid from "./Card/CardGrid.tsx";
 
-export { default as Card } from "./Card/Card.tsx";
-export { default as CardSkeleton } from "./Card/CardSkeleton.tsx";
-export { default as CardGrid } from "./Card/CardGrid.tsx";
+import Cart from "./Header/Cart/Cart.tsx";
 
-export { default as Cart } from "./Header/Cart/Cart.tsx";
+import Modal from "./Modal/Modal.tsx";
 
-export { default as Modal } from "./Modal/Modal.tsx";
+import CheckoutGrid from "./Checkout/CheckoutGrid.tsx";
+import createOrder from "./Checkout/createOrder.ts";
+import stripeCheckout from "./Checkout/stripeCheckout.ts";
 
-export { default as CheckoutGrid } from "./Checkout/CheckoutGrid.tsx";
-export { default as createOrder } from "./Checkout/createOrder.tsx";
-export { default as stripeCheckout } from "./Checkout/stripeCheckout.tsx";
+import ProductSlider from "./Slider/ProductSlider.tsx";
+import ProductSliderSkeleton from "./Slider/ProductSliderSkeleton.tsx";
+import Slider from "./Slider/Slider.tsx";
 
-export { default as ProductSlider } from "./Slider/ProductSlider.tsx";
-export { default as ProductSliderSkeleton } from "./Slider/ProductSliderSkeleton.tsx";
-export { default as Slider } from "./Slider/Slider.tsx";
+import ErrorWrapper from "./ErrorWrapper/ErrorWrapper.tsx";
 
-export { default as ErrorWrapper } from "./ErrorWrapper/ErrorWrapper.tsx";
+export {
+  SwiperSlide as Slide,
+  ProductGrid,
+  Swiper,
+  Form,
+  SignInForm,
+  SignUpForm,
+  Login,
+  UserAddressForm,
+  Card,
+  CardSkeleton,
+  CardGrid,
+  Cart,
+  Modal,
+  CheckoutGrid,
+  createOrder,
+  stripeCheckout,
+  ProductSlider,
+  ProductSliderSkeleton,
+  Slider,
+  ErrorWrapper,
+};
 
 export type { SliderProps };
