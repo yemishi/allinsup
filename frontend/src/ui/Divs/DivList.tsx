ba16923fd5c4c31a05e39b760dfba3d53ca7e066
yemishi <daviddperera10@gmail.com>
2025-04-29 14:07:02 +0100

    improved ui

--- frontend/src/ui/Divs/DivList.tsx
+++ frontend/src/ui/Divs/DivList.tsx
@@ -9,8 +9,8 @@ export default function DivList({ dd, dt, ...props }: ListPops) {
   const { className = "", ...rest } = props;
   return (
     <div {...rest} className={cleanClasses(className, "flex gap-1 w-full justify-between pb-2 0 px-2")}>
-      <dt>{dt}</dt>
-      <dd className="font-bold text-secondary-500">{dd}</dd>
+      <dt className="font-semibold">{dt}</dt>
+      <dd className="font-bold text-secondary-500 text-right">{dd}</dd>
     </div>
   );
 }
